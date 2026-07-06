aa26c2ce3567b4defb09527b12312e7491c9df69
Allistar Smith <allistarsmith98@gmail.com>
2026-07-06 01:05:41 -0400

    Add Gas Station minigame! :)

--- client/src/scenes/GasStationScene.ts
+++ client/src/scenes/GasStationScene.ts
@@ -1,8 +1,22 @@
 import Phaser from "phaser";
 import { Location, LocationDisplayName } from "../enums/Location";
+import { GameState } from "../state/GameState";
 import { addBackToMapButton } from "../components/BackToMapButton";
 
+
 export default class GasStationScene extends Phaser.Scene {
+  private gasText!: Phaser.GameObjects.Text;
+  private chancesText!: Phaser.GameObjects.Text;
+  private messageText!: Phaser.GameObjects.Text;
+
+  private activeCircle: Phaser.GameObjects.Arc | null = null;
+  private badCircles: Phaser.GameObjects.Arc[] = [];
+  private timerEvent: Phaser.Time.TimerEvent | null = null;
+
+  private chancesRemaining = 16;
+  private readonly maxGas = 16;
+  private readonly roundSeconds = 1.5;
+
   constructor() {
     super("GasStationScene");
   }
@@ -10,16 +24,113 @@ export default class GasStationScene extends Phaser.Scene {
   create() {
     this.cameras.main.setBackgroundColor("#ffffff");
 
-    this.add.text(450, 850, LocationDisplayName[Location.GAS], {
+    this.add.text(450, 80, LocationDisplayName[Location.GAS], {
       fontSize: "32px",
       color: "#000000",
     }).setOrigin(0.5);
 
-    this.add.text(450, 300, "Get gas for the food truck", {
-      fontSize: "28px",
-      color: "#000000",
-    }).setOrigin(0.5);
+    this.gasText = this.add.text(450, 150, "", {
+            fontSize: "28px",
+            color: "#000000",
+        }).setOrigin(0.5);
 
-    addBackToMapButton(this, 450, 500);
-  }
+        this.chancesText = this.add.text(450, 190, "", {
+            fontSize: "24px",
+            color: "#000000",
+        }).setOrigin(0.5);
+
+        this.messageText = this.add.text(450, 245, "Click the moving gas target!", {
+            fontSize: "24px",
+            color: "#000000",
+        }).setOrigin(0.5);
+
+        this.updateHud();
+        this.startRound();
+
+        addBackToMapButton(this, 450, 540);
+    }
+
+    private startRound() {
+        this.clearRound();
+
+        if (GameState.gallonsOfGas >= this.maxGas) {
+            this.endMiniGame("Tank full!");
+            return;
+        }
+
+        if (this.chancesRemaining <= 0) {
+            this.endMiniGame("Out of chances!");
+            return;
+        }
+
+        const x = Phaser.Math.Between(100, 800);
+        const y = Phaser.Math.Between(300, 450);
+
+        this.activeCircle = this.add.circle(x, y, 30, 0x00aa00)
+            .setInteractive({ useHandCursor: true });
+
+        for (let i = 0; i < 3; i++) {
+          const badX = Phaser.Math.Between(100, 800);
+          const badY = Phaser.Math.Between(300, 450);
+
+          const badCircle = this.add.circle(badX, badY, 25, 0xff9900)
+              .setInteractive({ useHandCursor: true });
+
+          badCircle.on("pointerdown", () => {
+              this.chancesRemaining -= 1;
+              this.messageText.setText("Oops! Bad target!");
+              this.updateHud();
+              this.startRound();
+          });
+
+          this.badCircles.push(badCircle);
+        }
+
+        this.activeCircle.on("pointerdown", () => {
+            GameState.gallonsOfGas = Math.min(
+                GameState.gallonsOfGas + 1,
+                this.maxGas
+            );
+
+            this.chancesRemaining -= 1;
+            this.messageText.setText("Nice! +1 gallon");
+            this.updateHud();
+            this.startRound();
+        });
+
+        this.timerEvent = this.time.delayedCall(this.roundSeconds * 1000, () => {
+            this.chancesRemaining -= 1;
+            this.messageText.setText("Too slow!");
+            this.updateHud();
+            this.startRound();
+        });
+    }
+
+    private clearRound() {
+        if (this.activeCircle) {
+            this.activeCircle.destroy();
+            this.activeCircle = null;
+        }
+
+        if (this.timerEvent) {
+            this.timerEvent.remove(false);
+            this.timerEvent = null;
+        }
+
+        for (const circle of this.badCircles) {
+          circle.destroy();
+        }
+
+        this.badCircles = [];
+    }
+
+    private updateHud() {
+        this.gasText.setText(`Gas: ${GameState.gallonsOfGas}/${this.maxGas} gal`);
+        this.chancesText.setText(`Chances left: ${this.chancesRemaining}`);
+    }
+
+    private endMiniGame(message: string) {
+        this.clearRound();
+        this.messageText.setText(message);
+    }
 }
